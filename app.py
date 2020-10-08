229b8ea05aff880515afa5adb5186be818a5c463
Lucy Zheng <lucy.zheng@utexas.edu>
2020-10-08 16:17:27 -0500

    Starting db implementation.

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from ncaam_bb_api import Player, Team, Game
+import pymongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient()
+db = client['cbidb']
 
+teamsDictionary = Team.populate()
+articles = db.articles
+# result = articles.insert(teamsDictionary)
 
 @app.route('/')
 def index():
@@ -12,4 +20,6 @@ def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
+    # print(articles.count())
+
     app.run(debug=True)
